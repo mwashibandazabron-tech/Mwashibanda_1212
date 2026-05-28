9a4e5a870b5305b90cc7d090b486f3a6f995c6e8
mwashibandazabron-tech <mwashibandazabron@gmail.com>
2026-05-28 23:30:27 +0300

    Add files via upload

---
+++ BrothersApplication.java
@@ -0,0 +1,13 @@
+package com.example.brothers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrothersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrothersApplication.class, args);
+    }
+
+}
